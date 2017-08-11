da925e1b0faf0a1b588609fed2aa69d1204a6be5
Paul Molins <paul.molins@metix.ca>
2017-08-11 16:40:59 -0400

    initialisation des migrations
    - Carte
    - API
    - Consultation

---
+++ phinx/db/migrations/bciti/20170811173013_api_migration.php
@@ -0,0 +1,46 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class ApiMigration extends AbstractMigration{
+
+	/**
+	 * Change Method.
+	 *
+	 * Write your reversible migrations using this method.
+	 *
+	 * More information on writing migrations is available here:
+	 * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+	 *
+	 * The following commands can be used in this method and Phinx will
+	 * automatically reverse them when rolling back:
+	 *
+	 *    createTable
+	 *    renameTable
+	 *    addColumn
+	 *    renameColumn
+	 *    addIndex
+	 *    addForeignKey
+	 *
+	 * Remember to call "create()" or "update()" and NOT "save()" when working
+	 * with the Table class.
+	 */
+	public function change(){
+
+		$this->vCreateApiDocTable();
+		$this->vCreateApiEndPointTable();
+		$this->vCreateApiVersionTable();
+	}
+
+	protected function vCreateApiDocTable(){
+
+	}
+
+	protected function vCreateApiEndPointTable(){
+
+	}
+
+	protected function vCreateApiVersionTable(){
+		
+	}
+}
