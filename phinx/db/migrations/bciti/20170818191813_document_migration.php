865734b02a024c695a2ec373bfcda74c7f4c9448
yomane <bruno.cipelletti@metix.ca>
2017-08-18 15:25:46 -0400

    BCITI-7512 fix Currency

---
+++ phinx/db/migrations/bciti/20170818191813_document_migration.php
@@ -0,0 +1,49 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class DocumentMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(){
+	
+	    $this->table('Document', [ 'id'=> false, 'primary_key' => [ 'id' ] ] )
+		    ->addColumn('id',                               'biginteger',   [ 'identity'  => true ] )
+		
+		    ->addColumn('fkiOrganisationId',            'biginteger',   [ 'null'    => false ] )
+		    ->addColumn('iTypeId',                      'biginteger',   [ 'null'    => false ] )
+		    ->addColumn('sName',                        'string',       [ 'null'    => false ] )
+		    ->addColumn('sDescription',                 'string',       [ 'null'    => false ] )
+		    ->addColumn('iExpirationTimeInSeconds',     'biginteger',   [ 'null'    => true  ] )
+		
+		
+		    ->addColumn('bDeleted',                     'integer',      [ 'null'    => false ,   'default' => 0 ] )
+		    ->addColumn('iCreation',                    'biginteger',   [ 'null'    => true ] )
+		    ->addColumn('iModification',                'biginteger',   [ 'null'    => true ] )
+		
+		    ->addIndex(     'bDeleted',                     [ 'name' => 'idx_deleted'] )
+		    ->addIndex(     'fkiOrganisationId',            [ 'name' => 'idx_organisation'] )
+		
+		    ->create();
+    }
+}
