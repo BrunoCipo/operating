8cf9da5b58d039b4a2e1fd8ec57015429293503a
yomane <bruno.cipelletti@metix.ca>
2017-08-23 15:42:33 -0400

    BCITI-7512 TokenApi

---
+++ phinx/db/migrations/bciti/20170823193725_token_api_migration.php
@@ -0,0 +1,43 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class TokenApiMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(){
+	
+	    $this->table('TokenApi', [ 'id'=> false ] )
+		
+		    ->addColumn('fkiUserId',                    'biginteger',   [ 'null'    => true  ] )
+		    ->addColumn('sToken',                       'string',       [ 'null'    => false,    'length' => 128, 'default' => ''  ] )
+		    ->addColumn('iExpiration',                  'biginteger',   [ 'null'    => true  ] )
+		
+		    ->addColumn('iCreation',                    'biginteger',   [ 'null'    => true  ] )
+		    ->addColumn('iModification',                'biginteger',   [ 'null'    => true  ] )
+		
+		    ->addIndex(     'fkiUserId',                     [ 'name' => 'fkiUserId' ] )
+		
+		    ->create();
+    }
+}
